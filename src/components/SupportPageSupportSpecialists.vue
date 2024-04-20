6ccaf0cbc818b801eaf212a038c9c9fc2f1a3e73
starik061 <starik061@gmail.com>
2024-04-20 12:17:32 +0300

    made Support Specialist section

--- src/components/SupportPageSupportSpecialists.vue
+++ src/components/SupportPageSupportSpecialists.vue
@@ -1,70 +1,96 @@
 <template>
-    <section class="commercial-author-section">
-        <div class="main-container">
+    <section class="support-specialists-section">
+        <div class="main-container text-wrapper">
             <h2 class="support-page-section-header section-header">Специалисты<br />
                 службы поддержки</h2>
-            <p>Пожалуйста, прежде, чем написать нам, ознакомьтесь с информацией<br />ниже и проверьте список вопросов
-                <strong>ЧаВо</strong>.<br />
+            <p class="instruction-text">Пожалуйста, прежде, чем написать нам, ознакомьтесь с информацией<br />ниже и
+                проверьте список вопросов
+                <a class="link-to-faq" href="#">ЧаВо</a>.<br />
                 Возможно, там уже есть нужный вам ответ
             </p>
-
-            <ul class="communication-rules-list">
-                <li class="communication-rules-list-item">
-                    <div class="communication-rules-icon-wrapper">
-                        <img src="/public/support-page-communication-guide-1.png" width="110" height="110"
-                            alt="communication guide icon">
-                    </div>
-                    <div>
-                        <b class="communication-rule-header">Подробно опишите свой вопрос</b>
-                        <p>Техподдержка часто получает «пустые» жалобы, в которых пользователь не дает комментария.
-                            Конечно,
-                            есть ряд очевидных ситуаций, где они не нужны. Но в большинстве случаев вопрос будет решен
-                            быстрее, если вы объясните, в чем именно он заключается.</p>
-                    </div>
+            <ul class="specialist-list">
+                <li class="specialist-list-item">
+                    <a class="specialist-list-item-link"
+                        href="https://author.today/Account/Login?ReturnUrl=%2Fpm#633210" target="_blank"
+                        rel="noopener noreferrer">
+                        <h3 class="specialist-name">Ольга</h3>
+                        <p class="specialist-job-title">Тех.поддержка и модерация</p>
+                        <ul>
+                            <li>Время работы: 08:00 - 20:00 (мск)</li>
+                            <li>Выходные: ПТ, СБ</li>
+                            <li>См. информацию «О себе»</li>
+                            <p class="write-to-private">Написать ЛС</p>
+                        </ul>
+                    </a>
                 </li>
-                <li class="communication-rules-list-item">
-                    <div class="communication-rules-icon-wrapper">
-                        <img src="/public/support-page-communication-guide-2.png" width="110" height="110"
-                            alt="communication guide icon">
-                    </div>
-                    <div>
-                        <b class="communication-rule-header">Не забудьте важные детали</b>
-                        <p>Мы ежедневно повышаем навыки телепатии наших сотрудников. Однако, до совершенства нам ещё
-                            далеко,
-                            потому, пожалуйста, всегда указывайте важные детали, особенно те, о которых мы знать просто
-                            не
-                            можем (личная информация, авторство арта и проч.)</p>
-                    </div>
+                <li class="specialist-list-item">
+                    <a class="specialist-list-item-link"
+                        href="https://author.today/Account/Login?ReturnUrl=%2Fpm#450598" target="_blank"
+                        rel="noopener noreferrer">
+                        <h3 class="specialist-name">Юми</h3>
+                        <p class="specialist-job-title">Тех.поддержка и модерация</p>
+                        <ul>
+                            <li>Время работы: 10:00 - 22:00 (мск)</li>
+                            <li>График работы 2/2.</li>
+                            <li>См. информацию «О себе»</li>
+                            <p class="write-to-private">Написать ЛС</p>
+                        </ul>
+                    </a>
                 </li>
-                <li class="communication-rules-list-item">
-                    <div class="communication-rules-icon-wrapper">
-                        <img src="/public/support-page-communication-guide-3.png" width="110" height="110"
-                            alt="communication guide icon">
-                    </div>
-                    <div>
-                        <b class="communication-rule-header">Укажите ссылку</b>
-                        <p>Если ваш вопрос связан с определенной публикацией (книга, запись в блоге, комментарий) либо с
-                            пользователем, пожалуйста, всегда указывайте прямую ссылку. Если ваш вопрос связан с 1 из
-                            1000
-                            комментариев под блогом, а ссылка ведёт на блог, мы не сможем быстро сориентироваться и
-                            разрешить ситуацию. Скорее всего, просто её не поймём.</p>
-                    </div>
+                <li class="specialist-list-item">
+                    <a class="specialist-list-item-link"
+                        href="https://author.today/Account/Login?ReturnUrl=%2Fpm#771836" target="_blank"
+                        rel="noopener noreferrer">
+                        <h3 class="specialist-name">Иннокентий</h3>
+                        <p class="specialist-job-title">Тех.поддержка и модерация</p>
+                        <ul>
+                            <li>Время работы: 10:00 - 22:00 (мск)</li>
+                            <li>График работы 2/2.</li>
+                            <li>См. информацию «О себе»</li>
+                            <p class="write-to-private">Написать ЛС</p>
+                        </ul>
+                    </a>
                 </li>
-                <li class="communication-rules-list-item">
-                    <div class="communication-rules-icon-wrapper">
-                        <img src="/public/support-page-communication-guide-4.png" width="110" height="110"
-                            alt="communication guide icon">
-                    </div>
-                    <div>
-                        <b class="communication-rule-header">Будьте вежливы!</b>
-                        <p>К сожалению, сотрудники технической поддержки ежедневно сталкиваются с неуважительным
-                            отношением.
-                            Многие пользователи путают нас с отделом психологической помощи, другие просто стремятся
-                            сорвать
-                            плохое настроение, третьи считают наших сотрудников кем-то вроде домовых эльфов, позволяют
-                            себе
-                            мат и переход на личности. Не надо так. <strong>Мы тоже люди.</strong></p>
-                    </div>
+                <li class="specialist-list-item">
+                    <a class="specialist-list-item-link"
+                        href="https://author.today/Account/Login?ReturnUrl=%2Fpm#1470410" target="_blank"
+                        rel="noopener noreferrer">
+                        <h3 class="specialist-name">Юлия</h3>
+                        <p class="specialist-job-title">Тех.поддержка и модерация</p>
+                        <ul>
+                            <li>Время работы: 08:00 - 16:00 (мск)</li>
+                            <li>Выходные: ВС, ПН</li>
+                            <li>См. информацию «О себе»</li>
+                            <p class="write-to-private">Написать ЛС</p>
+                        </ul>
+                    </a>
+                </li>
+                <li class="specialist-list-item">
+                    <a class="specialist-list-item-link"
+                        href="https://author.today/Account/Login?ReturnUrl=%2Fpm#209324" target="_blank"
+                        rel="noopener noreferrer">
+                        <h3 class="specialist-name">Марина Д.</h3>
+                        <p class="specialist-job-title">Вопросы по рекламе, акциям и настройке Яндекс.Метрики</p>
+                        <ul>
+                            <li>Время ответа на ЛС с 17-21:00 (мск)</li>
+                            <li>Выходные: СБ, ВС</li>
+                            <p class="write-to-private">Написать ЛС</p>
+                        </ul>
+                    </a>
+                </li>
+                <li class="specialist-list-item">
+                    <a class="specialist-list-item-link"
+                        href="https://author.today/Account/Login?ReturnUrl=%2Fpm#1828998" target="_blank"
+                        rel="noopener noreferrer">
+                        <h3 class="specialist-name">Дмитрий Малкин</h3>
+                        <p class="specialist-job-title">Вопросы поддержки авторов сайта (творческая акклиматизация на
+                            АТ)</p>
+                        <ul>
+                            <li>Время ответа на ЛС с 10-19:00 (мск)</li>
+                            <li>Выходные: СБ, ВС</li>
+                            <p class="write-to-private">Написать ЛС</p>
+                        </ul>
+                    </a>
                 </li>
             </ul>
         </div>
@@ -76,4 +102,77 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.support-specialists-section {
+    padding: 135px 0 60px;
+}
+
+.text-wrapper {
+    text-align: center;
+    line-height: 27px;
+}
+
+.instruction-text {
+    margin-bottom: 90px;
+}
+
+.link-to-faq {
+    font-weight: 700;
+    color: var(--decor-color-first);
+    border-bottom: 1px solid var(--decor-color-first);
+    cursor: pointer;
+}
+
+.specialist-list {
+    display: flex;
+    gap: 40px;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: flex-start;
+}
+
+.specialist-list-item {
+    flex-basis: 360px;
+    flex-shrink: 0;
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 10px 0px;
+    text-align: left;
+    cursor: pointer;
+}
+
+.specialist-list-item-link {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    padding: 29px 30px 40px;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 28px;
+    color: inherit;
+}
+
+.specialist-name {
+    margin-bottom: 11px;
+    font-size: 22px;
+    font-weight: 600;
+    line-height: 30px;
+}
+
+.specialist-job-title {
+    font-weight: 700;
+}
+
+.write-to-private {
+    margin-top: 18px;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 25px;
+
+    color: var(--decor-color-first);
+
+    &::after {
+        content: " →";
+
+    }
+}
+</style>
