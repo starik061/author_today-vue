291592db0818e203de52cf9b1912ddd8ee5c7cb5
starik061 <starik061@gmail.com>
2024-04-22 09:54:58 +0300

    configure Vueform lib for WriteUs section

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import Vueform from "@vueform/vueform";
+import vueformConfig from "./../vueform.config";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Vueform, vueformConfig);
 
 app.mount("#app");
